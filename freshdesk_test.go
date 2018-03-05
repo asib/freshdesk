1855ad3faefc6cec8b5709562764fdd25368e9a3
Silvan Jegen <silvan@smallpdf.com>
2018-03-05 16:56:41 +0100

    freshdesk_test: update test to new API

--- freshdesk_test.go
+++ freshdesk_test.go
@@ -1,31 +1,34 @@
 package freshdesk
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/leebenson/conform"
+)
 
 func TestTicket(t *testing.T) {
+	t.Skip("Need account info to test this")
 
-	// FILL IN YOUR INFORMATION AND UNCOMMENT TO TEST!
+	client, err := NewClient("", "")
+	if err != nil {
+		t.Fatalf("Could not create client: %s\n", err)
+	}
 
-	// r := &Request{
-	// 	Domain: "",
-	// 	API:    "",
-	// }
-	//
-	// tk := &NewTicket{}
-	// tk.Ticket = &Ticket{
-	// 	Email:       "",
-	// 	Name:        "",
-	// 	Subject:     "this is a test",
-	// 	Type:        "Question",
-	// 	Description: "the content of the ticket would go here",
-	// 	Status:      Open,
-	// 	Priority:    Medium,
-	// 	Source:      Portal,
-	// }
-	//
-	// conform.Strings(tk.Ticket)
-	//
-	// if e := tk.Create(r); e != nil {
-	// 	fmt.Println("error", e)
-	// }
+	ticket := &Ticket{
+		Email:       "",
+		Name:        "",
+		Subject:     "this is a test",
+		Type:        "Question",
+		Description: "the content of the ticket would go here",
+		Status:      Open,
+		Priority:    Medium,
+		Source:      Portal,
+	}
+
+	// optionally check the ticket with conform
+	conform.Strings(ticket)
+
+	if _, err := client.CreateTicket(ticket); err != nil {
+		t.Fatalf("failed to create ticket: %s", err)
+	}
 }
